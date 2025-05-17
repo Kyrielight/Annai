6bc68f0df7910a2606f1c67495e60b486e3f99d0
Kyrielight <jarek13705@gmail.com>
2025-05-17 15:55:32 +0900

    Add resolver for Google Maps

--- resolver/library.go
+++ resolver/library.go
@@ -4,14 +4,17 @@ import (
 	"moe.best.annai/resolver/implementations/core"
 	"moe.best.annai/resolver/implementations/dictionaries"
 	"moe.best.annai/resolver/implementations/social"
+	"moe.best.annai/resolver/implementations/transit"
 	"moe.best.annai/resolver/model"
 )
 
 var resolvers = map[string]model.Resolver{
 	"g":       core.GOOGLE(),
+	"gmaps":   transit.GOOGLE_MAPS(),
 	"google":  core.GOOGLE(),
 	"j":       dictionaries.JISHO(),
 	"jisho":   dictionaries.JISHO(),
+	"maps":    transit.GOOGLE_MAPS(),
 	"youtube": social.YOUTUBE(),
 	"yt":      social.YOUTUBE(),
 }
